5a6a4c62b370c6e362fc2642a9d2f2ca82eb44ca
toko202011 <toko202011@gmail.com>
2020-11-17 07:30:48 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
@@ -15,10 +17,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = 'ユーザーを登録しました。'
+      flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
     else
-      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
